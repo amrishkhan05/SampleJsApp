aef892625295b832f543a3b7192d50e806402bc4
Sheik Abdullah <593566@cognizant.com>
2018-09-27 17:41:05 +0530

    save changes

--- script.js
+++ script.js
@@ -1,21 +1,19 @@
 var score, roundScore, activePlayer;
-var temp = 0;
-score = [0, 0];
-roundScore = 0;
-activePlayer = 0;
+init();
 
+function init() {
+    score = [0, 0];
+    roundScore = 0;
+    activePlayer = 0;
+    document.getElementById('score-0').textContent = 0;
+    document.getElementById('score-1').textContent = 0;
+    document.getElementById('current-0').textContent = 0;
+    document.getElementById('current-1').textContent = 0;
+}
 // dice = Math.floor(Math.random() * 6) + 1;
 // console.log(dice)
 // document.querySelector('#current-' + activePlayer).textContent = dice;
-
-document.querySelector('.dice').style.display = 'none';
-document.getElementById('score-0').textContent = 0;
-document.getElementById('score-1').textContent = 0;
-document.getElementById('current-0').textContent = 0;
-document.getElementById('current-1').textContent = 0;
-
-
-
+// document.querySelector('.dice').style.display = 'none';
 document.querySelector('.btn-roll').addEventListener('click', function() {
     var dice = Math.floor(Math.random() * 6) + 1;
     var diceDOM = document.querySelector('.dice');
@@ -25,12 +23,30 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         roundScore = roundScore + dice;
         document.getElementById('current-' + activePlayer).textContent = roundScore;
     } else {
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-        roundScore = 0;
-        document.getElementById('current-0').textContent = 0;
-        document.getElementById('current-1').textContent = 0;
-        document.querySelector('.player-0-panel').classList.toggle('active');
-        document.querySelector('.player-1-panel').classList.toggle('active')
-
+        nextPlayer();
     }
+})
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    //add the current score to the global score
+    score[activePlayer] += roundScore;
+    //update score in ui
+    document.getElementById('score-' + activePlayer).textContent = score[activePlayer];
+    if (score[activePlayer] >= 20) {
+        document.getElementById('name-' + activePlayer).textContent = 'WINNER!';
+        document.querySelector('.dice').style.display = 'none';
+    } else {
+        nextPlayer();
+    }
+})
+
+function nextPlayer() {
+    activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+    roundScore = 0;
+    document.getElementById('current-0').textContent = 0;
+    document.getElementById('current-1').textContent = 0;
+    document.querySelector('.player-0-panel').classList.toggle('active');
+    document.querySelector('.player-1-panel').classList.toggle('active')
+}
+document.querySelector('.btn-new').addEventListener('click', function() {
+    init();
 })
